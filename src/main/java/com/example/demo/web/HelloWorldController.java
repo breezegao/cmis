316cb453e40f6a01d51f5ba5a181c7f6f34fbaf1
breeze <464558047.com>
2019-03-01 14:35:48 +0800

    页面输入框开发

--- src/main/java/com/example/demo/web/HelloWorldController.java
+++ src/main/java/com/example/demo/web/HelloWorldController.java
@@ -1,19 +1,38 @@
 package com.example.demo.web;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @SpringBootApplication
 public class HelloWorldController {
 
     //这里使用@RequestMapping注解表示该方法对应的二级上下文路径
     @RequestMapping(value = "/getUserByGet", method = RequestMethod.GET)
+    @ResponseBody()
     String getUserByGet(@RequestParam(value = "userName") String userName){
         return "Hello " + userName;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/index")
+    public String index(){
+        return "hello";
+    }
+    @RequestMapping(method = RequestMethod.POST, value = "/welcome")
+    @ResponseBody()
+    public String welcome(@RequestParam(value = "userName") String userName) {
+
+        //return "welcome";
+        return "Hello,"+userName;
+    }
+    @RequestMapping(  value = "/hello")
+    @ResponseBody()
+    public ModelAndView welcomes() {
+        ModelAndView mode = new ModelAndView();
+        mode.setViewName("hello");
+        return mode;
     }
 
 
