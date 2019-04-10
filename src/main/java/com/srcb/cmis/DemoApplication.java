7d99cabac53df8e52aa647644417ad5236fbf023
breeze <464558047.com>
2019-04-10 10:58:39 +0800

    部署发布-启动类更新(解决发布问题)

--- src/main/java/com/srcb/cmis/DemoApplication.java
+++ src/main/java/com/srcb/cmis/DemoApplication.java
@@ -1,13 +1,21 @@
 package com.srcb.cmis;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
 	}
 
 }
